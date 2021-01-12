ded6d557ef1aedc179548fe6eaa0063481472798
sharma.59 <sharma.59@iitjac.in>
2021-01-12 19:53:02 +0530

    commiting another time

--- Scientific_Calculator/app/src/main/java/com/example/scientific_calculator/MainActivity.java
+++ Scientific_Calculator/app/src/main/java/com/example/scientific_calculator/MainActivity.java
@@ -11,4 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    // all changes need to be appear here
 }
